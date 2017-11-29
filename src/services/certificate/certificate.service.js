900e3fc6a6b56a56a202cf5a2734278811702938
Dan Schrimpsher <dan.schrimpsher@gmail.com>
2017-11-28 22:50:53 -0600

    Basic certificates service.

---
+++ src/services/certificate/certificate.service.js
@@ -0,0 +1,27 @@
+// Initializes the `certificate` service on path `/certificates`
+const createService = require('feathers-mongodb');
+const hooks = require('./certificate.hooks');
+const filters = require('./certificate.filters');
+
+module.exports = function () {
+  const app = this;
+  const paginate = app.get('paginate');
+  const mongoClient = app.get('mongoClient');
+  const options = { paginate };
+
+  // Initialize our service with any options it requires
+  app.use('/certificates', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('certificates');
+
+  mongoClient.then(db => {
+    service.Model = db.collection('certificate');
+  });
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,7 @@
+const certificate = require('./certificate/certificate.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(certificate);
+  app.configure(users);
 };
