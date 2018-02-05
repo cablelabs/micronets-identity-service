bc6d7b3b3ac048562fa15744b8e91021beef3c9d
Dan Schrimpsher <d.schrimpsher@cablelabs.com>
2018-02-05 12:16:33 -0600

    moved certificate generation to /certificates

--- src/hooks/create-cert.js
+++ src/hooks/create-cert.js
@@ -1,28 +1,71 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-var selfsigned = require('selfsigned');
+var forge = require('node-forge');
+var fs = require('fs');
+var path = require('path');
 
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function createCert (hook) {
-    var attrs = [{ name: 'commonName', value: 'contoso.com' }];
-    var pems = selfsigned.generate(attrs, { days: 365 });
-    console.log(pems)
+    // Read CSR
+    var csrPem = fs.readFileSync(path.join(__dirname, "../../sandbox/", "micronet.csr"));
+    var csr = forge.pki.certificationRequestFromPem(csrPem);
+
+// Read CA cert and key
+    var caCertPem = fs.readFileSync(path.join(__dirname, "../../sandbox/", "ca.pem"));
+    var caKeyPem = fs.readFileSync(path.join(__dirname, "../../sandbox/", "ca.key"));
+    var caCert = forge.pki.certificateFromPem(caCertPem);
+    var caKey = forge.pki.privateKeyFromPem(caKeyPem);
+
+    if (csr.verify()) {
+      console.log('Certification request (CSR) verified.');
+    } else {
+      throw new Error('Signature not verified.');
+    }
 
 
-// The authenticated user
-//     const user = hook.params.user;
-    // The actual message text
-    // const text = hook.data.text
-    // Messages can't be longer than 400 characters
-    //   .substring(0, 400)
-      // Do some basic HTML escaping
-      // .replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    console.log('Creating certificate...');
+    var cert = forge.pki.createCertificate();
+    cert.serialNumber = '02';
 
-    // Override the original data
-    hook.data =  pems;
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
+    cert.validity.notBefore = new Date();
+    cert.validity.notAfter = new Date();
+    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+
+
+// subject from CSR
+    cert.setSubject(csr.subject.attributes);
+// issuer from CA
+    cert.setIssuer(caCert.subject.attributes);
+
+    cert.setExtensions([{
+      name: 'basicConstraints',
+      cA: true
+    }, {
+      name: 'keyUsage',
+      keyCertSign: true,
+      digitalSignature: true,
+      nonRepudiation: true,
+      keyEncipherment: true,
+      dataEncipherment: true
+    }, {
+      name: 'subjectAltName',
+      altNames: [{
+        type: 6, // URI
+        value: 'http://example.org/webid#me'
+      }]
+    }]);
+
+    cert.publicKey = csr.publicKey;
+
+
+    cert.sign(caKey);
+    console.log('Certificate created.');
+
+    console.log('\nWriting Certificate');
+
+    hook.result = forge.pki.certificateToPem(cert);
+
     return Promise.resolve(hook);
   };
 };
